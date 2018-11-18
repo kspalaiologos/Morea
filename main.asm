f2810fa440e0092fccf1243ea03cc88a82f74c68
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2018-11-18 18:23:15 +0100

    General doublestream open and close.

--- main.asm
+++ main.asm
@@ -142,20 +142,52 @@ l6:
 	?+8; sp ?+4; bp; 0 bp; inc sp
 	?+8; sp ?+4; ?+7; 0 ?+3; Z Z 0
 
-_fclose_slot1:
+_fopen:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; bp 0
 	bp; sp bp
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t1 0
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t2 0
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t3 0
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t4 0
 
-	c8 (-1)
+	t2; t1; bp t2; c5 t2; t2 t1
+	t2; t4; ?+11; t1 Z; Z ?+4; Z; 0 t2; t2 t4
+	t1; Z t4 ?+3; Z Z ?+9
+	t4 Z ?+3; Z Z ?+3; inc t1
+	Z t1 l7
+	t1; t2; bp t1; c8 t1; t1 t2
+	t1; t3; ?+11; t2 Z; Z ?+4; Z; 0 t1; t1 t3
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+9; sp ?+5; t3 Z; Z 0; Z
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; ?+2 0 _fopen_slot1; . ?;
+	c5 sp
 
-	c2 (-1)
+	Z Z l8
+l7:
+	t1; t2; bp t1; c8 t1; t1 t2
+	t1; t3; ?+11; t2 Z; Z ?+4; Z; 0 t1; t1 t3
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+9; sp ?+5; t3 Z; Z 0; Z
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; ?+2 0 _fopen_slot2; . ?;
+	c5 sp
+l8:
 
+	?+8; sp ?+4; t4; 0 t4; inc sp
+	?+8; sp ?+4; t3; 0 t3; inc sp
+	?+8; sp ?+4; t2; 0 t2; inc sp
+	?+8; sp ?+4; t1; 0 t1; inc sp
 	sp; bp sp
 	?+8; sp ?+4; bp; 0 bp; inc sp
 	?+8; sp ?+4; ?+7; 0 ?+3; Z Z 0
 
-_fclose_slot2:
+_fclose_slot1:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; bp 0
 	bp; sp bp
@@ -168,6 +200,56 @@ _fclose_slot2:
 	?+8; sp ?+4; bp; 0 bp; inc sp
 	?+8; sp ?+4; ?+7; 0 ?+3; Z Z 0
 
+_fclose_slot2:
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; bp 0
+	bp; sp bp
+
+	c10 (-1)
+
+	c2 (-1)
+
+	sp; bp sp
+	?+8; sp ?+4; bp; 0 bp; inc sp
+	?+8; sp ?+4; ?+7; 0 ?+3; Z Z 0
+
+_fclose:
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; bp 0
+	bp; sp bp
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t1 0
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t2 0
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t3 0
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t4 0
+
+	t2; t3; bp t2; c5 t2; t2 t3
+	t2; t4; ?+11; t3 Z; Z ?+4; Z; 0 t2; t2 t4
+	t3; Z t4 ?+3; Z Z ?+9
+	t4 Z ?+3; Z Z ?+3; inc t3
+	Z t3 l9
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; ?+2 0 _fclose_slot1; . ?;
+	inc sp
+
+	Z Z l10
+l9:
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; ?+2 0 _fclose_slot2; . ?;
+	inc sp
+l10:
+
+	?+8; sp ?+4; t4; 0 t4; inc sp
+	?+8; sp ?+4; t3; 0 t3; inc sp
+	?+8; sp ?+4; t2; 0 t2; inc sp
+	?+8; sp ?+4; t1; 0 t1; inc sp
+	sp; bp sp
+	?+8; sp ?+4; bp; 0 bp; inc sp
+	?+8; sp ?+4; ?+7; 0 ?+3; Z Z 0
+
 _fgetc_slot1:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; bp 0
@@ -175,7 +257,7 @@ _fgetc_slot1:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; t1 0
 
-	c10 (-1)
+	c11 (-1)
 
 	c2 (-1)
 
@@ -200,7 +282,7 @@ _fputc_slot1:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; t3 0
 
-	c11 (-1)
+	c12 (-1)
 
 	t1; t2; bp t1; c5 t1; t1 t2
 	t1; t3; ?+11; t2 Z; Z ?+4; Z; 0 t1; t1 t3
@@ -222,7 +304,7 @@ _fgetc_slot2:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; t1 0
 
-	c12 (-1)
+	c13 (-1)
 
 	c2 (-1)
 
@@ -247,7 +329,7 @@ _fputc_slot2:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; t3 0
 
-	c13 (-1)
+	c14 (-1)
 
 	t1; t2; bp t1; c5 t1; t1 t2
 	t1; t3; ?+11; t2 Z; Z ?+4; Z; 0 t1; t1 t3
@@ -269,7 +351,7 @@ _feof_slot1:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; t1 0
 
-	c14 (-1)
+	c15 (-1)
 
 	c2 (-1)
 
@@ -290,7 +372,7 @@ _feof_slot2:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; t1 0
 
-	c15 (-1)
+	c16 (-1)
 
 	c2 (-1)
 
@@ -311,7 +393,7 @@ _ftell_slot1:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; t1 0
 
-	c16 (-1)
+	c17 (-1)
 
 	c2 (-1)
 
@@ -332,7 +414,7 @@ _ftell_slot2:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; t1 0
 
-	c17 (-1)
+	c18 (-1)
 
 	c2 (-1)
 
@@ -347,32 +429,6 @@ end_ftell_slot2:
 	?+8; sp ?+4; ?+7; 0 ?+3; Z Z 0
 
 _fseek_slot1:
-	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
-	?+6; sp ?+2; bp 0
-	bp; sp bp
-	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
-	?+6; sp ?+2; t1 0
-	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
-	?+6; sp ?+2; t2 0
-	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
-	?+6; sp ?+2; t3 0
-
-	c18 (-1)
-
-	t1; t2; bp t1; c5 t1; t1 t2
-	t1; t3; ?+11; t2 Z; Z ?+4; Z; 0 t1; t1 t3
-	t3 (-1)
-
-	c2 (-1)
-
-	?+8; sp ?+4; t3; 0 t3; inc sp
-	?+8; sp ?+4; t2; 0 t2; inc sp
-	?+8; sp ?+4; t1; 0 t1; inc sp
-	sp; bp sp
-	?+8; sp ?+4; bp; 0 bp; inc sp
-	?+8; sp ?+4; ?+7; 0 ?+3; Z Z 0
-
-_fseek_slot2:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; bp 0
 	bp; sp bp
@@ -398,6 +454,32 @@ _fseek_slot2:
 	?+8; sp ?+4; bp; 0 bp; inc sp
 	?+8; sp ?+4; ?+7; 0 ?+3; Z Z 0
 
+_fseek_slot2:
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; bp 0
+	bp; sp bp
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t1 0
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t2 0
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t3 0
+
+	c20 (-1)
+
+	t1; t2; bp t1; c5 t1; t1 t2
+	t1; t3; ?+11; t2 Z; Z ?+4; Z; 0 t1; t1 t3
+	t3 (-1)
+
+	c2 (-1)
+
+	?+8; sp ?+4; t3; 0 t3; inc sp
+	?+8; sp ?+4; t2; 0 t2; inc sp
+	?+8; sp ?+4; t1; 0 t1; inc sp
+	sp; bp sp
+	?+8; sp ?+4; bp; 0 bp; inc sp
+	?+8; sp ?+4; ?+7; 0 ?+3; Z Z 0
+
 _fsize_slot1:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; bp 0
@@ -405,7 +487,7 @@ _fsize_slot1:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; t1 0
 
-	c20 (-1)
+	c21 (-1)
 
 	c2 (-1)
 
@@ -426,7 +508,7 @@ _fsize_slot2:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; t1 0
 
-	c21 (-1)
+	c22 (-1)
 
 	c2 (-1)
 
@@ -453,23 +535,23 @@ _unlink:
 	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
 	?+6; sp ?+2; t4 0
 
-	c22 (-1)
+	c23 (-1)
 
-l7:
+l11:
 	t1; t2; bp t1; c5 t1; t1 t2
 	t1; t3; ?+11; t2 Z; Z ?+4; Z; 0 t1; t1 t3
 	t2; t1; ?+11; t3 Z; Z ?+4; Z; 0 t2; t2 t1
 	t3; t1 Z; Z t3 ?+3; Z Z ?+9; Z; t1 t3; t1 t3
-	Z t3 l9
+	Z t3 l13
 	t2; t1; bp t2; c5 t2; t2 t1
 	t2; t4; ?+11; t1 Z; Z ?+4; Z; 0 t2; t2 t4
 	t1; t2; ?+11; t4 Z; Z ?+4; Z; 0 t1; t1 t2
 	t2 (-1)
-l8:
+l12:
 	t1; t2; bp t1; c5 t1; t1 t2
 	t2 Z; ?+9; Z ?+5; Z; inc 0
-	Z Z l7
-l9:
+	Z Z l11
+l13:
 
 	c2 (-1)
 
@@ -486,7 +568,7 @@ _fflush_slot1:
 	?+6; sp ?+2; bp 0
 	bp; sp bp
 
-	c23 (-1)
+	c24 (-1)
 
 	c2 (-1)
 
@@ -499,10 +581,31 @@ _fflush_slot2:
 	?+6; sp ?+2; bp 0
 	bp; sp bp
 
-	c24 (-1)
+	c25 (-1)
 
 	c2 (-1)
 
+	sp; bp sp
+	?+8; sp ?+4; bp; 0 bp; inc sp
+	?+8; sp ?+4; ?+7; 0 ?+3; Z Z 0
+
+_getchar:
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; bp 0
+	bp; sp bp
+	dec sp; ?+11; sp ?+7; ?+6; sp ?+2; 0
+	?+6; sp ?+2; t1 0
+
+	c26 (-1)
+
+	c2 (-1)
+
+	t1; (-1) t1
+	ax; t1 ax
+	Z Z end_getchar
+
+end_getchar:
+	?+8; sp ?+4; t1; 0 t1; inc sp
 	sp; bp sp
 	?+8; sp ?+4; bp; 0 bp; inc sp
 	?+8; sp ?+4; ?+7; 0 ?+3; Z Z 0
@@ -522,7 +625,7 @@ sqmain:
 
 	Z Z (-1)
 
-. c5:-2 c2:0 c24:10 c6:11 c7:12 c8:13 c9:14 c10:15 c11:16 c12:17 c13:18 c14:19 c15:20 c4:21 c1:22 c3:23 c16:31 c17:32 c18:33 c19:34 c20:35 c21:36 c22:8 c23:9
+. c5:-2 c8:-3 c2:0 c25:10 c6:11 c7:12 c9:13 c10:14 c11:15 c12:16 c13:17 c14:18 c15:19 c16:20 c4:21 c1:22 c3:23 c17:31 c18:32 c19:33 c20:34 c21:35 c22:36 c26:37 c23:8 c24:9
 
 . t1:0 t2:0 t3:0 t4:0
 
